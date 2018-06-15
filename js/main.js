878c1a37db9bb6bc2dfc78e5c01a57353881deba
lostimage <aisovik87@gmail.com>
2018-06-15 22:45:06 +0300

    adaptive added

--- js/main.js
+++ js/main.js
@@ -2,7 +2,56 @@ $(document).ready(function(){
   $('.ba-slider').slick({
   	vertical:true,
   	dots:true,
-  	arrows:false
-  });
+		arrows:false,
+		asNavFor:'.ba-intro__bg-wood-slider',
+		appendDots:'.ba-slider-wrap',
+		responsive: [
+			{
+				breakpoint: 960,
+				settings: {
+					vertical:false,
+					infinite: true,
+					dots: true,
+					
+				}
+			}
+		]
+	});
+	$('.ba-intro__bg-wood-slider').slick({
+		vertical:true,
+	verticalSwiping:true,
+  	dots:false,
+		arrows:false,
+		asNavFor:'.ba-slider'
+	});
+/// slow link scroll
+
+	$(".ba-intro").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	/// mobile menu 
+	var nav = $('.ba-mobile-nav') ,
+			overlay = $('.ba-overlay'),
+			wrap = $('.ba-wrap'),
+	menuToggle = $('.ba-menu-button, .ba-overlay') ;
+	menuToggle.on('click' , function (e) {
+		e.preventDefault();
+		nav.toggleClass('ba-open');
+		overlay.toggleClass('ba-open-overlay');
+		wrap.toggleClass('ba-open');
+	});
+
+	// init magnific-popup
+	$('[data-modal-video]').magnificPopup({
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 310
+		
+	});
+
 });
 
